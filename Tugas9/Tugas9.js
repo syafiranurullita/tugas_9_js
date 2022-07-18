64f174e39abcf6ebc0abe48fd8e9b24a83b36585
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 19:05:07 +0700

    tugas 9 js
    untuk Niomic

---
+++ Tugas9/Tugas9.js
@@ -0,0 +1,13 @@
+function panggilObject(){
+  var profil = {
+    nama : "Syafira",
+    umur : 21,
+    Hobi : "dengerin musik",
+    olahraga : "karate",
+    }
+    for(var x in profil){
+      console.log(profil[x])
+    }
+  }
+
+panggilObject();
